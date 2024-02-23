42b0da94b6759afbc355b552e9b5ee07b2b6510c
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2024-02-23 17:32:41 +0700

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from database import SessionLocal, engine, Base
+from routers import user as UserRouter
+import uvicorn
+
+# Создание всех таблиц в базе данных
+Base.metadata.create_all(bind=engine)
+
+# Инициализация FastAPI
+app = FastAPI()
+app.include_router(UserRouter.router, prefix='/user')
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8080, reload=True, workers=4)
---
+++ app/readme.md
@@ -0,0 +1,15 @@
+# local
+
+pip install fastapi uvicorn sqlalchemy alembic
+
+pip freeze > requirements.txt
+
+python main.py
+
+# docker
+
+## build docker image
+docker build --no-cache -t crud_fastapi:latest .
+
+## run docker container
+docker run -p 8080:8080 --name=crud_fastapi -d crud_fastapi
